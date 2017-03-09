1aa36656cd0c8d0bf779df54d8b8502bf3989ed4
Douglas Metthey <d.metthey@gmail.com>
2017-03-09 10:06:49 +0100

    init symfony

---
+++ src/Kipskool/Bundle/NewsBundle/Controller/DefaultController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Kipskool\Bundle\NewsBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class DefaultController extends Controller
+{
+    public function indexAction()
+    {
+        return $this->render('NewsBundle:Default:index.html.twig');
+    }
+}
---
+++ src/Kipskool/Bundle/NewsBundle/NewsBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Kipskool\Bundle\NewsBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class NewsBundle extends Bundle
+{
+}
