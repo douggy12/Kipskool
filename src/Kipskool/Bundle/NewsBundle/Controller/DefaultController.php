3518b29206e2092779385a8ce6db1030be76cb2d
Douglas Metthey <d.metthey@gmail.com>
2017-03-09 10:21:34 +0100

    comit de test

--- src/Kipskool/Bundle/NewsBundle/Controller/DefaultController.php
+++ src/Kipskool/Bundle/NewsBundle/Controller/DefaultController.php
@@ -6,8 +6,11 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class DefaultController extends Controller
 {
+    /**
+     * @Route("/", name="Home")
+     */
     public function indexAction()
     {
-        return $this->render('NewsBundle:Default:index.html.twig');
+        return $this->render('default/index.html.twig');
     }
 }
