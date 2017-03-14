771b8a84a21cd91ce22d3642de166a4d5704cd63
Douglas Metthey <d.metthey@gmail.com>
2017-03-14 15:39:07 +0100

    Init Sonata Admin + integration Promo + article Promo Admin

---
+++ src/Kipskool/Bundle/Admin/ArticlesPromoAdmin.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Kipskool\Bundle\Admin;
+
+use Sonata\AdminBundle\Admin\AbstractAdmin;
+use Sonata\AdminBundle\Datagrid\DatagridMapper;
+use Sonata\AdminBundle\Datagrid\ListMapper;
+use Sonata\AdminBundle\Form\FormMapper;
+
+class ArticlesPromoAdmin extends AbstractAdmin{
+
+    protected function configureFormFields(FormMapper $formMapper){
+        $formMapper
+            ->add('titre', 'text')
+            ->add('texte','textarea')
+            ->add('src_feature','text')
+            ->add('Promo', 'entity', array(
+                'class' => 'Kipskool\Bundle\NewsBundle\Entity\Promo',
+                'choice_label' => 'nom',
+            ))
+        ;
+
+    }
+
+    protected function configureDatagridFilters(DatagridMapper $datagridMapper){
+        $datagridMapper
+            ->add('titre')
+            ->add('promo', null, array(), 'entity', array(
+                'class' => 'Kipskool\Bundle\NewsBundle\Entity\Promo',
+                'choice_label' => 'nom',
+            ))
+        ;
+
+    }
+
+    protected function configureListFields(ListMapper $listMapper){
+        $listMapper
+            ->addIdentifier('titre')
+            ->add('promo.nom')
+            ->add('createdAt', 'date', array(
+                'pattern' => 'MM dd Y G',
+
+                'timezone' => 'Europe/Paris'
+            ))
+
+        ;
+    }
+}
