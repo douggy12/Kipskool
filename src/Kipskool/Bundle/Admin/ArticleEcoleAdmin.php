6e5cac24373bc252626ce99b8d705e22aff46d79
allan <all.brochard@gmail.com>
2017-03-15 09:56:05 +0100

    EcoleAdmin add and modify ArticlePersoAdmin

--- src/Kipskool/Bundle/Admin/ArticleEcoleAdmin.php
+++ src/Kipskool/Bundle/Admin/ArticleEcoleAdmin.php
@@ -3,26 +3,28 @@ namespace Kipskool\Bundle\Admin;
 
 use Sonata\AdminBundle\Admin\AbstractAdmin;
 use Sonata\AdminBundle\Datagrid\ListMapper;
-use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Sonata\AdminBundle\Datagrid\DatagridMapper;
 use Sonata\AdminBundle\Form\FormMapper;
 
-class ArticlePersoAdmin extends AbstractAdmin
+class ArticleEcoleAdmin extends AbstractAdmin
 {
     protected function configureFormFields(FormMapper $formMapper){
         $formMapper
             ->add('titre', 'text')
             ->add('texte', 'textarea')
-            ->add('src_feature', 'text')
-            ->add('Perso', 'entity', array(
-                'class'=>'Kipskool\Bundle\NewsBundle\Entity\Perso',
-                'choice_label'=>'nom'));
+            ->add('srcFeature', 'text')
+            ->add('ecole', 'entity', array(
+                'class'=> 'Kipskool\Bundle\NewsBundle\Entity\Ecole',
+                'choice_label'=>'nom'
+            ))
+        ;
     }
 
     protected function configureDatagridFilters(DatagridMapper $datagridMapper){
         $datagridMapper
-            ->add('Perso', null, array(), 'entity', array(
-                'class'=> 'Kipskool\Bundle\NewsBundle\Entity\Perso',
+            ->add('titre')
+            ->add('ecole', null, array(), 'entity', array(
+                'class'=> 'Kipskool\Bundle\NewsBundle\Entity\Ecole',
                 'choice_label'=>'nom'
             ));
     }
@@ -30,12 +32,14 @@ class ArticlePersoAdmin extends AbstractAdmin
     protected function configureListFields(ListMapper $listMapper){
         $listMapper
             ->addIdentifier('titre')
+            ->add('texte')
+            ->add('srcFeature')
+            ->add('ecole.nom')
             ->add('createdAt', 'date', array(
-                'pattern'=>'dd MM YYYY',
-                'locale'=>'fr',
-                'timezone'=>'Europe/Paris'
+                    'pattern'=>'dd MM YYYY',
+                    'locale'=>'fr',
+                    'timezone'=>'Europe/Paris'
             ))
-            ->add('perso.nom')
-            ->add('texte');
+        ;
     }
 }
