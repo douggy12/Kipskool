f9f57c854edc4e0836852956a2053e3a05302c89
allan <all.brochard@gmail.com>
2017-03-16 17:02:49 +0100

    add my function filter but still have a problem

--- src/Kipskool/Bundle/NewsBundle/Twig/AppExtension.php
+++ src/Kipskool/Bundle/NewsBundle/Twig/AppExtension.php
@@ -20,4 +20,25 @@ class AppExtension extends \Twig_Extension
 
         return $price;
     }
+//class TwigExtension extends \Twig_Extension
+//{
+//    public function getRaccourci(){
+//        return array(
+//            new \Twig_SimpleFilter('text_raccourci', array($this, 'textFilter'))
+//        );
+//    }
+//
+//    public function textFilter($chaine)
+//    {
+//        $positionDernierEspace = 0;
+//        if( strlen($chaine) >= 50)
+//        {
+//            $chaine = substr($this,0,50);
+//            $positionDernierEspace = strrpos($chaine,' ');
+//            $chaine = substr($chaine,0,$positionDernierEspace).'...';
+//        }
+//        $chaine= '['.$chaine.']';
+//        return $chaine;
+//    }
+//}
 }
