52e56c3ef172f86013be1c4fa1540548792e3d68
Douglas Metthey <d.metthey@gmail.com>
2017-03-30 10:09:11 +0200

    mis à jour des requires

--- src/NewsBundle/Entity/Ecole.php
+++ src/NewsBundle/Entity/Ecole.php
@@ -7,6 +7,7 @@ use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\HttpFoundation\File\File;
 use Symfony\Component\Validator\Context\ExecutionContextInterface;
 use Vich\UploaderBundle\Mapping\Annotation as Vich;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * Ecole
