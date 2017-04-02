d7297234a99e700a39495cacccf5a5fe965f7259
Douglas Metthey <d.metthey@gmail.com>
2017-04-02 15:57:19 +0200

    ajout de la fonction rechercher perso en ajax

---
+++ src/NewsBundle/Controller/AjaxRechercherController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace NewsBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Serializer\Encoder\JsonEncode;
+
+class AjaxRechercherController extends Controller
+{
+    /**
+     * @param Request $request
+     * @return \Symfony\Component\HttpFoundation\Response
+     * @Method("POST")
+     * @Route("/rechercher")
+     */
+    public function indexAction(Request $request)
+    {
+        $motcle = $request->get('motcle');
+        $em = $this->getDoctrine()->getManager();
+        $query = $em->getRepository('NewsBundle:Perso')->findWithMotCle($motcle);
+//            ->select('p')
+//            ->where('p.prenom LIKE :motcle')
+//            ->setParameter('motcle',  '%'.$motcle.'%')
+//            ->orWhere('p.nom LIKE :motcle')
+//            ->setParameter('motcle', '%'.$motcle.'%')
+//            ->getQuery()
+//        ;
+        $resultats = $query->getResult();
+
+        $jsonList = array();
+
+        foreach ($resultats as $resultat){
+            $jsonList[] = array(
+               "prenom" => $resultat->getPrenom(),
+               "nom" => $resultat->getNom(),
+                "link" => '/perso/'.$resultat->getId()
+
+            );
+        }
+
+
+        return new JsonResponse($jsonList);
+    }
+}
