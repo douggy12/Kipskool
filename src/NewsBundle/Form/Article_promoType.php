cf424c55f89364c732ff755309322d441565155f
allan <all.brochard@gmail.com>
2017-04-03 17:01:16 +0200

    modify the crud and fix bug

--- src/NewsBundle/Form/Article_promoType.php
+++ src/NewsBundle/Form/Article_promoType.php
@@ -14,10 +14,11 @@ class Article_promoType extends AbstractType
      */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $builder->add('titre')->add('texte')->add('srcFeature', FileType::class, array(
+        $builder->add('titre')->add('texte', 'textarea')->add('srcFeature', FileType::class, array(
             'data_class' => null,
             'required' => false
         ));
+
     }
     
     /**
